38bbd927d648f2627e6152b1319cae83221d920d
mathf <Mathias>
2023-08-22 18:43:58 -0300

    facebook pixel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
 export default class MyDocument extends Document {
    render() {
@@ -28,6 +29,37 @@ export default class MyDocument extends Document {
                <link rel="preconnect" href="https://fonts.gstatic.com" />
                <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
                <link rel="shortcut icon" href="/static/img/logo.png" />
+               <React.Fragment>
+                  {/* <!-- Facebook Pixel Code --> */}
+                  <script dangerouslySetInnerHTML={{
+                     __html: `!function(f,b,e,v,n,t,s)
+               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+               n.queue=[];t=b.createElement(e);t.async=!0;
+               t.src=v;s=b.getElementsByTagName(e)[0];
+               s.parentNode.insertBefore(t,s)}(window, document,'script',
+               'https://connect.facebook.net/en_US/fbevents.js');
+               fbq('init', '700239298517091');
+               fbq('track', 'PageView');` }}
+                  />
+                  <noscript dangerouslySetInnerHTML={{
+                     __html: `<img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=700239298517091&ev=PageView&noscript=1" />` }}
+                  />
+                  {/* <!-- End Facebook Pixel Code --> */}
+                  {/* <!-- Facebook Pixel Code --> */}
+                  <script dangerouslySetInnerHTML={{
+                     __html: `
+               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+               })(window,document,'script','dataLayer','GTM-TGLDLST');
+               ` }}
+                  />
+                  {/* <!-- End Facebook Pixel Code --> */}
+               </React.Fragment>
             </Head>
             <body>
                <noscript
@@ -39,7 +71,7 @@ export default class MyDocument extends Document {
                />
                <Main />
                <NextScript />
-         </body>
+            </body>
          </Html >
       )
    }
